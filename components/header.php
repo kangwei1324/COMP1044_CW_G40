c942cd11eaf74ecfba08783a663526cb117495e4
Kwokym111 <kwok.yen.ming06@gmail.com>
2026-04-23 01:03:03 +0800

    updated components to be dynamic

--- components/header.php
+++ components/header.php
@@ -1,6 +1,6 @@
 <header class="top-nav">
     <div class="user-profile">
-        Welcome, Admin User
+        Welcome, <?= htmlspecialchars($_SESSION['fullname'] ?? 'User'); ?>
     </div>
     <a href="../auth/logout.php" class="logout-btn">Logout</a>
 </header>
--- components/header_assessor.php
+++
@@ -1,6 +0,0 @@
-<header class="top-nav">
-    <div class="user-profile">
-        Welcome, Assessor
-    </div>
-    <a href="../auth/logout.php" class="logout-btn">Logout</a>
-</header>
