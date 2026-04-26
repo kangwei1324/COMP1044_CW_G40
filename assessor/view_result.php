bd016b70bd17a3714c52317dea5e3796faf026f3
Kwokym111 <kwok.yen.ming06@gmail.com>
2026-04-26 15:38:40 +0800

    added minor missing code segments and removed old comments

--- assessor/view_result.php
+++ assessor/view_result.php
@@ -1,9 +1,4 @@
 <?php
-    // Error display for debugging
-    //ini_set('display_errors', 1);
-    //ini_set('display_startup_errors', 1);
-    //error_reporting(E_ALL);
-
     $required_role = 'assessor';
     include '../config/db.php';
     include '../includes/auth_check.php';
